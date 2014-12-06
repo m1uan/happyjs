c678c2dafbb8efef2ad25ff8986580337435445a
m1uan <milan.medlik@gmail.com>
2014-12-06 19:52:32 +1100

    hello world

---
+++ examples/simple-ctrl/ctrl/IndexCtrl.js
@@ -0,0 +1,34 @@
+/**
+ * Created by miuan on 6/12/14.
+ */
+module.exports = (function(){
+"use strict"
+    var self = {};
+
+    /** optional global seting
+     * self.$init = {
+     *      return {
+     *              // potentional config
+     *      }
+     * }
+     */
+
+    /** option setting just for function
+     * self.index_get_setting = {
+     *      // potentional setting
+     * }
+     */
+
+    /** option input parrams
+     * self.index_get_params: {
+     * }
+     */
+
+    self.index_get = function(request, reply){
+        console.log(reply);
+        reply('hello world');
+    }
+
+
+    return self;
+})();
